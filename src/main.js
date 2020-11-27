9c84e7091a98eb413c45dfe4a7a970d8d810c0de
nowLesgo <2501762293@qq.com>
2020-11-27 18:51:46 +0800

    feat:完成header组件注册、登录、搜索跳转

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "./style/reset.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-  router,
+  render: h => h(App),
+  router
 }).$mount("#app");
